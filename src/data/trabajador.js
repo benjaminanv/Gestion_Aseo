856273f289db0f39445d22a85e5c678f7da92f00
benjaminanv <benjaminanv@gmail.com>
2026-09-16 02:34:35 -0300

    datos e informacion de trabajadores

--- src/data/trabajador.js
+++ src/data/trabajador.js
@@ -1,8 +1,52 @@
 export const Trabajador = [
-    idTrabajador, 
-    nombre, 
-    rut, 
-    salasdesignadas, 
-    inicioturno, 
-    finturno
-                        ]    
+    {
+    idTrabajador: 1,
+    nombre: "Juan Pérez",
+    rut: "12.345.678-9",
+    salasdesignadas: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    inicioturno: "08:00",
+    finturno: "16:00"
+    },
+    {
+    idTrabajador: 2,
+    nombre: "María López",
+    rut: "9.876.543-2",
+    salasdesignadas: [11,12,13,14,15,16,17,18,19,20],
+    inicioturno: "08:00",
+    finturno: "16:00"
+    },
+    {
+    idTrabajador: 3,
+    nombre: "Ruperto Flores",
+    rut: "7.231.321-5",
+    salasdesignadas: [21,22,23,24,25,26,27,28,29],
+    inicioturno: "08:00",
+    finturno: "16:00"
+    },
+    {
+    idTrabajador: 4,
+    nombre: "Gabriel Diaz",
+    rut: "9.221.902-2",
+    salasdesignadas: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    inicioturno: "16:00",
+    finturno: "23:00"
+    },
+    {
+    idTrabajador: 5,
+    nombre: "Juana Fernadez",
+    rut: "8.532.521-8",
+    salasdesignadas: [11,12,13,14,15,16,17,18,19,20],
+    inicioturno: "16:00",
+    finturno: "23:00"
+    },
+    {
+    idTrabajador: 6,
+    nombre: "Rosa Gonzalez",
+    rut: "11.226.842-4",
+    salasdesignadas: [21,22,23,24,25,26,27,28,29],
+    inicioturno: "16:00",
+    finturno: "23:00"
+    }
+    
+
+]
