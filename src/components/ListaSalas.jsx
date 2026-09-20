8f4017ab1da442dd2619fc42de711b11fc9d43f8
benjaminanv <benjaminanv@gmail.com>
2026-09-20 19:17:58 -0300

    implementacion de .map

--- src/components/ListaSalas.jsx
+++ src/components/ListaSalas.jsx
@@ -5,6 +5,9 @@ function ListaSalas() {
     <div>
       {salas.map((sala) => (
         <div key={sala.idSala}>
+          <p>Piso: {sala.piso}</p>
+          <p>Estado: {sala.estado}</p>
+          <p>Última limpieza: {sala.horaUltimaLimpieza}</p>  
           <p>Sala: {sala.numero}</p>
         </div>
       ))}
