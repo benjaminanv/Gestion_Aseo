b49222ec09698a5ad403ee7f3f88835924e4c013
benjaminanv <benjaminanv@gmail.com>
2026-09-20 19:21:23 -0300

    separacion de salas por pisos

--- src/components/ListaSalas.jsx
+++ src/components/ListaSalas.jsx
@@ -3,11 +3,42 @@ import { salas } from '../data/salas'
 function ListaSalas() {
   return (
     <div>
-      {salas.map((sala) => (
+      <h2>Piso 1</h2>
+      {salas.filter((sala) => sala.piso === 1).map((sala) => (
         <div key={sala.idSala}>
           <p>Piso: {sala.piso}</p>
           <p>Estado: {sala.estado}</p>
-          <p>Última limpieza: {sala.horaUltimaLimpieza}</p>  
+          <p>Última limpieza: {sala.horaUltimaLimpieza}</p>
+          <p>Sala: {sala.numero}</p>
+        </div>
+      ))}
+
+      <h2>Piso 2</h2>
+      {salas.filter((sala) => sala.piso === 2).map((sala) => (
+        <div key={sala.idSala}>
+          <p>Piso: {sala.piso}</p>
+          <p>Estado: {sala.estado}</p>
+          <p>Última limpieza: {sala.horaUltimaLimpieza}</p>
+          <p>Sala: {sala.numero}</p>
+        </div>
+      ))}
+
+      <h2>Piso 3</h2>
+      {salas.filter((sala) => sala.piso === 3).map((sala) => (
+        <div key={sala.idSala}>
+          <p>Piso: {sala.piso}</p>
+          <p>Estado: {sala.estado}</p>
+          <p>Última limpieza: {sala.horaUltimaLimpieza}</p>
+          <p>Sala: {sala.numero}</p>
+        </div>
+      ))}
+
+      <h2>Piso 4</h2>
+      {salas.filter((sala) => sala.piso === 4).map((sala) => (
+        <div key={sala.idSala}>
+          <p>Piso: {sala.piso}</p>
+          <p>Estado: {sala.estado}</p>
+          <p>Última limpieza: {sala.horaUltimaLimpieza}</p>
           <p>Sala: {sala.numero}</p>
         </div>
       ))}
