78eafd59672801efbae5c0dd18cbd4eb15e93b7f
benjaminanv <benjaminanv@gmail.com>
2026-09-20 20:15:01 -0300

    creacion de funcion nombreEstado

--- src/components/ListaSalas.jsx
+++ src/components/ListaSalas.jsx
@@ -16,7 +16,12 @@ function colorEstado(estado) {
   if (estado === 'pendiente') return 'danger'
   return 'warning'
 }
-
+function nombreEstado(estado) {
+    if (estado === 'en_proceso') return 'En proceso'
+    if (estado === 'limpio') return 'Limpio'
+    if (estado === 'pendiente') return 'Pendiente'
+    return estado
+}
 function ListaSalas() {
   const [listaSalas, setListaSalas] = useState(salasIniciales)
   const [pisoActivo, setPisoActivo] = useState(1)
