ae5bf8e731ee1e763699ba5c82147783e3a1d34d
benjaminanv <benjaminanv@gmail.com>
2026-09-21 22:23:27 -0300

    creacion de 3er componente TrabajadorTag.jsx

---
+++ src/components/TrabajadorTag.jsx
@@ -0,0 +1,20 @@
+import { Trabajador } from '../data/trabajador'
+
+function TrabajadorTag({ idTrabajador }) {
+  const trabajador = Trabajador.find((t) => t.idTrabajador === idTrabajador)
+
+  if (!trabajador) return null
+
+  return (
+    <span className="trabajador-tag">
+      {idTrabajador}
+      <span className="trabajador-tooltip">
+        <strong>{trabajador.nombre}</strong>
+        <br />
+        Turno: {trabajador.inicioturno} - {trabajador.finturno}
+      </span>
+    </span>
+  )
+}
+
+export default TrabajadorTag
