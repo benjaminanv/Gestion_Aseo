029d85f98c3b7df4ff1838c6db31ed3a155dfabb
benjaminanv <benjaminanv@gmail.com>
2026-09-24 14:43:24 -0300

    creacion de conexion de login con app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import ListaSalas from './components/ListaSalas' 
+import { useState } from 'react'
+import ListaSalas from './components/ListaSalas'
+import Login from './components/Login'
+
+
 
 function App() {
+  const [usuarioActual, setUsuarioActual] = useState(null)
+
+  if (!usuarioActual) {
+    return <Login onIniciarSesion={setUsuarioActual} />
+  }
+
   return (
     <div>
-    
-      <ListaSalas />
+      <ListaSalas usuarioActual={usuarioActual} onCerrarSesion={() => setUsuarioActual(null)} />
     </div>
   )
 }
-
-
-
 export default App
